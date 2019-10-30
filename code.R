6cddc9135c38f350bff122459ad567ef6430c27c
Minghao Liu <35540144+mhliu0419@users.noreply.github.com>
2019-10-30 16:58:19 -0400

    Update code.R

--- code.R
+++ code.R
@@ -1,5 +1,9 @@
+
+# Load the data for all players performance statistics
+
 batting <- read.csv('/Users/AncelotLiu/Desktop/R-Course-HTML-Notes/R-for-Data-Science-and-Machine-Learning/Training\ Exercises/Capstone\ and\ Data\ Viz\ Projects/Capstone\ Project/Batting.csv')
 
+# To show the data of batting
 
 head(batting)
 
@@ -7,6 +11,9 @@ str(batting)
 
 head(batting$AB)
 head(batting$X2B)
+
+
+# Adding new columns to batting data
 
 
 batting$BA <- batting$H / batting$AB
@@ -23,6 +30,8 @@ batting$SLG <- ((1 * batting$X1B) + (2 * batting$X2B) + (3 * batting$X3B) + (4 *
 str(batting)
 
 
+# Load the data of player salary
+
 
 sal <- read.csv('/Users/AncelotLiu/Desktop/R-Course-HTML-Notes/R-for-Data-Science-and-Machine-Learning/Training\ Exercises/Capstone\ and\ Data\ Viz\ Projects/Capstone\ Project/Salaries.csv')
 
@@ -33,9 +42,27 @@ batting <- subset(batting, yearID >= 1985)
 summary(batting)
 
 
+# Combine batting data with salary data according to playerID and year
+
+
 combo <- merge(batting,sal,by = c('playerID', 'yearID'))
 
 summary(combo)
+
+
+# To only show the three lost players' data in 2001
+
+
+lost_players <- subset(combo, playerID %in% c('giambja01','damonjo01','saenzol01'))
+lost_players
+
+lost_players <- subset(lost_players, yearID == 2001)
+lost_players <- lost_players[,c('playerID', 'H', 'X2B','X3B','HR','OBP','SLG','BA','AB')]
+
+
+lost_players
+
+
 
 
 
