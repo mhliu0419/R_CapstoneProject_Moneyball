bf6d0451c5ac185fb7a66edb214e1e5e8181b5a6
Minghao Liu <35540144+mhliu0419@users.noreply.github.com>
2019-10-29 20:51:25 -0400

    Update code.R

--- code.R
+++ code.R
@@ -6,3 +6,14 @@ str(batting)
 
 head(batting$AB)
 head(batting$X2B)
+
+
+batting$BA <- batting$H / batting$AB
+tail(batting$BA,5)
+
+
+batting$OBP <- (batting$H + batting$BB + batting$HBP)/(batting$AB + batting$BB + batting$BB + batting$HBP)
+
+batting$X1B <- batting$H - batting$X2B - batting$X3B - batting$HR
+
+batting$SLG <- ((1 * batting$X1B) + (2 * batting$X2B) + (3 * batting$X3B) + (4 * batting$HR) ) / batting$AB
